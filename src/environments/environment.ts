16e8a008ada03fe9ad326e4f31e51368d4049af9
Andomahefa <ando6651493523@gmail.com>
2022-04-07 00:54:09 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://locavfdvdfbvdflhost:3001/api'
+  apiBaseUrl: 'http://localhost:3001/api',
+  production: true
 };
 
 /*
